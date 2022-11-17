e312279da5c1f797d5601f8ebb8d55f2f44516fd
YY-nb <916295483@qq.com>
2022-11-17 23:03:29 +0800

    06.Grab Update Two Hands Grab and Dynamic Attach

--- Assets/Scripts/06.Grab/GunController.cs
+++ Assets/Scripts/06.Grab/GunController.cs
@@ -10,30 +10,12 @@ public class GunController : MonoBehaviour
     public Transform spawnPoint;
     public float fireSpeed = 40;
 
-    private Transform leftHandAttachPoint;
-    private Transform rightHandAttachPoint;
     private XRGrabInteractable grabbable;
 
     void Start()
     {
-        leftHandAttachPoint = transform.Find("LeftHand Attach Point");
-        rightHandAttachPoint = transform.Find("RightHand Attach Point");
         grabbable = GetComponent<XRGrabInteractable>();
-        grabbable.selectEntered.AddListener(ChangeAttachTransform);
         grabbable.activated.AddListener(FireBullet);
-    }
-
-    private void ChangeAttachTransform(SelectEnterEventArgs arg)
-    {
-        Transform interactor = arg.interactorObject.transform; 
-        if (interactor.name == "LeftHand Controller")
-        {
-            grabbable.attachTransform = leftHandAttachPoint;
-        }
-        else if (interactor.name == "RightHand Controller")
-        {
-            grabbable.attachTransform = rightHandAttachPoint;
-        }
     }
 
     private void FireBullet(ActivateEventArgs arg)
